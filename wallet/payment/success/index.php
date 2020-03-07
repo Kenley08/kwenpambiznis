ed2fb6841f9245f170ac8e96a686f3bba85b44c0
Eustache Luckens Yadley <eustache455@gmail.com>
2020-03-07 15:51:22 -0500

    modification payment success

--- wallet/payment/success/index.php
+++ wallet/payment/success/index.php
@@ -6,7 +6,7 @@ require_once'../../../api/Dao/transactionDao.php';
 $tran=new transaction();
 $tran->idtran="158301035050";
 $tran->transactionid="121212121213";
-
+//
 transactionDao::UpdateEtatTransactionId($tran);
 if(isset($tran->idtran) && isset($tran->transactionid)){
   echo "Li update";
