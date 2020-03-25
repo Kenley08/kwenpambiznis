d358e65288a0f14065ae3244db6e5d1ad9a54744
Eustache Luckens Yadley <eustache455@gmail.com>
2020-03-25 18:28:04 -0400

    åjout paiement produit

---
+++ file/payment_product.inc.php
@@ -0,0 +1,96 @@
+<form class="row " id="midle-new-ads" action="?" method="POST">
+    <div class="col-lg-7 col-xs-7 col-md-12">
+        <h2>T&egrave;m ak kondisyon</h2> <br> 
+        <div class="col col-md-12 bg-white padding-3" > 
+            Kwenpam pran tout mezi sekirite pou jere done ou yo. Ou lib pou ou verifye tout fason nou jere sa pandan wap aprann plis 
+            <a href="https://kwenpam.com/business/privacy/">politik konfidansyalite</a> nou yo. 
+        </div> <br>
+        <p class="title-page">
+            ID : <a href="#">23232323</a>  <br>
+            Deskipsyon : Laptop Dell, 4GB RAM, LD
+        </p>  
+        <div class="col col-md-12">
+            <p>
+                <h4>Pa ki mwayen ou konte peye?</h4> 
+            </p>
+            <div class="form-check">
+                <div class="radio">
+                    <label for="radio1" class="form-check-label">
+                        <input type="radio" id="radio1" name="radios1" value="option1" class="form-check-input" checked>Moncash
+                    </label>
+                </div>
+                <div class="radio">
+                    <label for="radio2" class="form-check-label">
+                        <input type="radio" id="radio2" name="radios1" value="option2" class="form-check-input">Bous kwenpam (<a href="https://kwenpam.com/business/legal/wallet">Aprann plis</a>)
+                    </label>
+                </div> 
+            </div>  <br> 
+            <p>
+                <h4>&Egrave;ske ou vle yo livre ou li?</h4> 
+            </p>
+            <div class="form-check">
+                <div class="radio">
+                    <label for="radio4" class="form-check-label ">
+                        <input type="radio" id="radio4" name="radios" value="option4" class="form-check-input" checked>Wi 
+                    </label>
+                </div>
+                <div class="radio">
+                    <label for="radio3" class="form-check-label ">
+                        <input type="radio" id="radio3" name="radios" value="option3" class="form-check-input" checked>Non (Map pase pran l)
+                    </label>
+                </div> 
+            </div>   <br> 
+            <p>
+                <h4>Enf&ograve;masyon itil</h4>
+            </p>
+            <div class="row form-group">
+                <div class="col col-md-3">
+                    <label for="txtnom" class=" form-control-label">Non konpl&egrave;</label>
+                </div>
+                <div class="col-12 col-md-9">
+                    <input type="text" id="txtnom" placeholder="EX : Junior Wesley"  class="form-control" required>
+                </div>
+            </div>
+            <div class="row form-group">
+                <div class="col col-md-3">
+                    <label for="txtemail" class=" form-control-label">Imel</label>
+                </div>
+                <div class="col-12 col-md-9">
+                    <input type="email" id="txtemail" placeholder="EX : nom@gmail.com"  class="form-control" required>
+                </div>
+            </div> 
+            <div class="row form-group">
+                <div class="col col-md-3">
+                    <label for="txttelephone" class=" form-control-label">Nimewo telef&ograve;n</label>
+                </div>
+                <div class="col-12 col-md-9">
+                    <input type="text" id="txttelephone" placeholder="EX: 47239232" class="form-control" required>
+                </div>
+            </div>
+            <div class="row form-group">
+                <div class="col col-md-3">
+                    <label for="txtville" class=" form-control-label">S&egrave;ks</label>
+                </div>
+                <div class="col-12 col-md-9">
+                    <select id="txtville" name="txtville" class="form-control" >
+                        <option value="0">Delma</option> 
+                    </select>
+                </div>
+            </div>
+            <div class="row form-group">
+                <div class="col col-md-3">
+                    <label for="txtadresse" class=" form-control-label">Adr&egrave; konpl&egrave;</label>
+                </div>
+                <div class="col-12 col-md-9">
+                    <input type="text" id="txtadresse" placeholder="117, Carrefour-feuille, route des dalles"   class="form-control" required>
+                </div>
+            </div>
+        </div> <br> 
+        <label for="checkbox1" class="form-check-label  ">
+            <input type="checkbox" id="checkbox1" name="checkbox1" value="option1" class="form-check-input"> 
+            Mwen byen li e konprann tout sa mwen sot f&egrave; la yo. Byen verifye enf&ograve;masyon yo avan menm ou peye, <a href="https://kwenpam.com/business/legal/terms">t&egrave;m ak kondisyon</a> aplikab.
+        </label> <br> <br>  
+        <a href="https://kwenpam.com/annonce/details.php"  class="btn btn-secondary btn-small">Anile </a> 
+        <input type="submit" class="btn btn-primary btn-small" value="Peye kounya" name="btnvalide"/> 
+    </div> 
+</form>
---
+++ wallet/payment/product/index.php
@@ -0,0 +1,45 @@
+<?php
+  session_start();  
+  if(isset($_GET['idan'])){
+      $_SESSION['id_an_pay']=$_GET['idan'];
+  }
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="kwenpam">
+    <title>Kwenpam | Tablo b&ograve; | Bous | Peye | Pwodwi</title>
+     <!-- Fontfaces CSS-->
+    <link href="../../../css/font-face.css" rel="stylesheet" media="all">
+    <link href="../../../vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <link href="../../../vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+    <link href="../../../vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+
+    <!-- Bootstrap CSS-->
+    <link href="../../../vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
+
+    <!-- Vendor CSS-->
+    <link href="../../../vendor/animsition/animsition.min.css" rel="stylesheet" media="all"> 
+
+    <!-- Main CSS-->
+    <link href="../../../css/theme.css" rel="stylesheet" media="all">
+    <link href="../../../css/main.css?v=233" rel="stylesheet" media="all">
+</head>
+<body class="animsition">
+    <div class="page-wrapper">
+         <?php
+            include '../../../file/payment_product.inc.php';
+         ?>
+    </div>
+    <!-- Jquery JS-->
+    <script src="../../../vendor/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap JS-->
+    <script src="../../../vendor/bootstrap-4.1/popper.min.js"></script>
+    <script src="../../../vendor/bootstrap-4.1/bootstrap.min.js"></script>
+    <script src="../../../vendor/animsition/animsition.min.js"></script> 
+    <script src="../../../vendor/perfect-scrollbar/perfect-scrollbar.js"></script>  
+    <script src="../../../js/main.js"></script>
+</body>
+</html>
